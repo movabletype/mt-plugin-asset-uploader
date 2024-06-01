87f48c65301b281043d3b3c4d9a2adc080349be6
Taku Amano <taku@taaas.jp>
2024-06-01 13:35:33 +0900

    Tweaks header.tmpl

--- plugins/AssetUploader/lib/MT/Plugin/AssetUploader/App/CMS.pm
+++ plugins/AssetUploader/lib/MT/Plugin/AssetUploader/App/CMS.pm
@@ -9,19 +9,23 @@ use warnings;
 use utf8;
 
 use MT::Util qw(encode_html);
-use Class::Method::Modifiers qw(install_modifier);
 use MT::Plugin::AssetUploader qw(plugin);
 
 sub template_param_header {
     my ($cb, $app, $param, $tmpl) = @_;
+    my $plugin = plugin();
     my $js_includes = $tmpl->getElementsByName("js_include")
         or return;
     my $before = $js_includes->[0];
-    my $tokens = plugin()->load_tmpl("header.tmpl")->tokens;
+
+    my $tokens = $plugin->load_tmpl("header.tmpl")->tokens;
     foreach my $t (@$tokens) {
         $tmpl->insertBefore($t, $before);
         $before = $t;
     }
+
+    my $t = $tmpl->createElement('Var', { name => $plugin->name . '_version', value => $plugin->version });
+    $tmpl->insertAfter($t, $before);
 }
 
 1;
