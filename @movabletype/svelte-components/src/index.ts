0438fde4361e4e73aeffa755eac46ec326a1fe63
Taku Amano <taku@taaas.jp>
2024-06-01 11:17:56 +0900

    Introduce @movabletype/svelte-component

---
+++ @movabletype/svelte-components/src/index.ts
@@ -0,0 +1,4 @@
+export { default as ComponentList } from "./ComponentList.svelte";
+export { default as HtmlElement } from "./HtmlElement.svelte";
+export { default as Modal } from "./Modal.svelte";
+export { default as ModalContent } from "./ModalContent.svelte";
